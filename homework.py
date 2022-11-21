f4f488ff4fe3bcc918cdf64cca2e91513db635e4
Bahram Mammad <baxram1991@outlook.com>
2022-11-21 21:36:11 +0400

    'homework'

---
+++ homework.py
@@ -0,0 +1,14 @@
+#number=((2+10)*3-10)
+#print(number**5)
+
+#a=input("eded:")
+#if a%3==0:
+    #print("bolunur")
+#else:
+    #print("bolunmur")
+
+#print ("""i don`t need "capuccino" or something else""")
+#print ('ha '*200)
+#print(not 'z' in 'python')
+#print('Men ' + "Python " + "oyrenirem")
+#print(type(6/3))
