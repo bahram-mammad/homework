10a73abbec08d3bd5bdfdd834227bd81e0bd4ba4
Bahram Mammad <baxram1991@outlook.com>
2022-11-24 10:35:00 +0400

    homework2

---
+++ homework2.py
@@ -0,0 +1,13 @@
+#val='92019017'.rjust(16, '*')
+#print(val)
+#print(divmod(15, 4)); print(3**3)
+#print(round(256.91872, -1)); print(round(256.91872, 1))
+#print('0'*3 + '25')
+
+#x=168
+#if x%8==0 and x%3==0 and x%7==0:
+    #print('duzdur')
+#else:
+    #print('sehvdir')
+
+
