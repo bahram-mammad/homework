c98da93191fc5b313661b94919e1b6d6611ba5d2
Bahram Mammad <baxram1991@outlook.com>
2022-11-26 00:44:49 +0400

    'homework3'

---
+++ homework3.py
@@ -0,0 +1,37 @@
+#name = (input('your name: '))
+#if name[8:40]:
+    #print('true')
+#elif name.isascii():
+    #print('true')
+#elif name.isalnum():
+    #print('true')
+#elif name.isupper() and name.islower():
+    #print('true')
+#elif name.isidentifier():
+    #print('true')
+#else:
+    #print('false')
+
+
+
+name = (input('your name: '))
+if name[8:40]:
+    print('true')
+else:
+    print('false')
+if name.isascii():
+    print('true')
+else:
+    print('false')
+if name.isalnum():
+    print('true')
+else:
+    print('false')
+if name.isupper():
+    print('true')
+else:
+    print('false')
+if name.isidentifier():
+    print('true')
+else:
+    print('false')
