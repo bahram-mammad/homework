458bd34f23daa6e8c2cb89943ea8a1f768593848
Bahram Mammad <baxram1991@outlook.com>
2022-12-08 00:26:49 +0400

    'homework4'

---
+++ homework4.py
@@ -0,0 +1,75 @@
+#1
+
+# ferma = ['at', 'qoyun', 'inek', 'at', 'inek', 'qoyun', 'at', 'at', 'keci']
+
+# print(ferma.index('keci'))
+
+# ferma.sort()
+# print(ferma)
+
+# print(dir(list))
+# ferma.remove('at')
+# ferma.append('toyuq')
+# print(ferma)
+
+# ferma.insert(0, 'keci')
+# print(ferma)
+
+# ferma.pop(4)
+# ferma.pop(5)
+# ferma.pop(6)
+# ferma.pop(3)
+# print(ferma)
+
+# elave = ['at', 'qoyun', 'inek', 'inek', 'qoyun']
+
+# ferma.append(elave)
+# print(ferma)
+
+# print((ferma)*3)
+
+# ferma.reverse()
+# print(ferma)
+
+# print(ferma.count('inek')/len(ferma)*100)
+
+# another = ferma.copy()
+# print(another)
+
+# ferma.clear()
+# print(ferma)
+
+
+#3
+# code = input()
+# list = []
+# for i in code:
+#     list.append(int(i))
+# list.reverse()
+# print(list)
+
+
+#4
+# numbers = [10.2, -32, -5.45, 32, 7, 43, -24.06, -4, -6.33, 62]
+# max = max(numbers)
+# min = min(numbers)
+# print(min,max)
+
+
+#2
+# result = [0, 0]
+# numbers = [10.2, -32, -5.45, 32, 7, 43, -24.06, -4, -6.33, 62]
+ 
+# musbet = 0
+# menfi = 0
+
+# for i in numbers:
+#     if i>0:
+#         musbet+=i
+#     elif i<0:
+#         menfi+=i
+
+# result.clear()
+# result.insert(0, musbet)
+# result.insert(1, menfi)
+# print(result)
