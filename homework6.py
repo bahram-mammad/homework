6d035043bd99174d46c0dbbb1b82846afd807b23
Bahram Mammad <baxram1991@outlook.com>
2022-12-24 02:17:23 +0400

    'homework6'

--- homework6.py
+++ homework6.py
@@ -1,5 +1,5 @@
-# # input: Men Javascript oyrenirem
-# # output: 13,5,14, 10,1,22,1,19,3,18,9,16,20, 15,25,18,5,14,9,18,5,13,
+#1) input: Men Javascript oyrenirem
+# output: 13,5,14, 10,1,22,1,19,3,18,9,16,20, 15,25,18,5,14,9,18,5,13,
 
 # text = input('metni daxil et:' )
 # numbers = [
@@ -7,3 +7,6 @@
 # ]
 # print(numbers)
 
+#3) text = input('metni daxil edin:' )
+# result = ''.joins(format(ord(i), '08b') for i in text)
+# print(result)
