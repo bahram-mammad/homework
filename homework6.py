ed2700340488ded48e7e6986b48e87f7daff93ed
Bahram Mammad <baxram1991@outlook.com>
2022-12-24 01:47:13 +0400

    'homework6'

---
+++ homework6.py
@@ -0,0 +1,9 @@
+# # input: Men Javascript oyrenirem
+# # output: 13,5,14, 10,1,22,1,19,3,18,9,16,20, 15,25,18,5,14,9,18,5,13,
+
+# text = input('metni daxil et:' )
+# numbers = [
+#     ord(char) - 96 for char in text.lower() 
+# ]
+# print(numbers)
+
