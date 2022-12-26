e5792e36e29eb343db532d8cfb9c36f827a81996
Bahram Mammad <baxram1991@outlook.com>
2022-12-27 02:49:28 +0400

    'homework7'

---
+++ homework7.py
@@ -0,0 +1,26 @@
+
+# 1)
+# n1 = 15 
+# n2 = 13
+# result = ('%s + %s = %s' % (n1, n2, n1+n2))
+# print(result)
+
+
+#2)
+# userData = [
+#     {
+#         'debt': 12543,
+#         'payed': 341.346742,
+#         'payed_percent': 0.027214122777644904,
+#         'card_number': '5326-6644-1234-6432',
+#         'first_name': 'Akif',
+#         'last_name': 'Serifov',
+#         'father_name': 'Elekber',
+#     }
+# ]
+
+# result = """Hormetli A. E. Serifov, sizin {:*<19} nomreli kredit kartiniza {:.2f} odenis edildi.
+# Umumi {}AZN teskil eden borcunuzdan {:.2%} borc silinmisdir!
+# """.format('5326-6644-1234-6432'[:9], 341.346742, 12543, 0.027214122777644904)
+
+# print(result)
